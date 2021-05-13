2fc4de617dbd9db545f62ed2643bb95be59775c0
Diego Cerqueira <xdiegocerqueira@gmail.com>
2021-05-13 17:40:08 -0300

    translating the index page

--- pages/index.js
+++ pages/index.js
@@ -9,32 +9,31 @@ export default function Home() {
         {/* Home heading */}
         <div className="home__content">
           <h1>RxC QV</h1>
-          <h2>An easy tool to host a quadratic vote.</h2>
+          <h2> Uma ferramenta fácil para hospedar uma votação quadrática. </h2>
           <p>
-            Quadratic Voting is the mathematically optimal way to vote in a
-            democratic community. Vote through collective decision-making, by
-            allocating votes that express the <i>degree</i> of your preferences,
-            not just <i>direction</i>.
+            A votação quadrática é a maneira matematicamente ideal de votar em uma comunidade democrática. 
+            Vote por meio de tomada de decisão coletiva, alocando votos que expressem o <i> grau </i> 
+            de suas preferências, não apenas <i> direção </i>
           </p>
-          <h2>Host a Quadratic Voting Event Below!</h2>
+          <h2> Organize um evento de votação quadrática abaixo! </h2>
         </div>
 
         {/* Home buttons */}
         <div className="home__cta">
           <div className="home__cta_button">
             <img src="/vectors/create_event.svg" alt="Create event" />
-            <h2>Create an event</h2>
-            <p>Set up Quadratic Voting for your event.</p>
-            <Link href="/create">
-              <a>Set Up Event</a>
-            </Link>
+            <h2> Crie um evento </h2>
+             <p> Configure a votação quadrática para o seu evento.</p>
+             <Link href = "/create">
+               <a> Configurar evento </a>
+             </Link>
           </div>
           <div className="home__cta_button">
             <img src="/vectors/place_vote.svg" alt="Place vote" />
-            <h2>Place your vote</h2>
-            <p>Use your secret code to place votes.</p>
-            <Link href="/place">
-              <a>Place Votes</a>
+            <h2> Dê o seu voto </h2>
+             <p> Use seu código secreto para votar.</p>
+             <Link href = "/place">
+               <a> Colocar votos </a>
             </Link>
           </div>
         </div>
