0ab0bd72a1e2be513547a76f2ae392cdc7bab3cc
alexrandaccio <acrandaccio@gmail.com>
2021-07-26 14:11:12 -0400

    check bearer token

--- pages/api/events/callback.js
+++ pages/api/events/callback.js
@@ -2,85 +2,106 @@ import prisma from "db"; // Import prisma
 
 // --> /api/events/callback
 export default async (req, res) => {
-  const data = req.body;
-  // TODO: check bearer token
-  const headers = req.headers;
-
   const crypto = require('crypto');
+  const data = req.body;
+  // const auth = req.headers.authorization;
 
-  // TESTING ONLY - MUDAMOS CLIENT BEHAVIOR
-  const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
-    modulusLength: 4096,
-    publicKeyEncoding: {
-      type: 'spki',
-      format: 'pem'
-    },
-    privateKeyEncoding: {
-      type: 'pkcs8',
-      format: 'pem',
-      cipher: 'aes-256-cbc',
-      passphrase: 'top secret'
+  const toBase64 = value => Buffer.from(value, "utf8").toString("base64");
+
+  const auth = 'Bearer ' + toBase64(process.env.APP_SECRET); // TESTING ONLY
+
+  const isValidAuth = (value, secret) => {
+    const givenSecret = String(value).split(' ')[1];
+
+    if (!givenSecret || givenSecret.length !== secret.length) {
+      return false;
     }
-  });
 
-  const sign = crypto.createSign('SHA256');
-  sign.write(data.hash);
-  sign.end();
-  const signature = sign.sign({
-    key: privateKey,
-    passphrase: 'top secret',
-  }, 'hex');
-  // END TESTING CODE
+    return crypto.timingSafeEqual(
+      Buffer.from(givenSecret, "base64"),
+      Buffer.from(secret, "base64")
+    );
+  };
 
-  const user_id = data.message.split(';')[0];
+  const secret = toBase64(process.env.APP_SECRET);
 
-  // Collect voter information
-  const user = await prisma.voters.findUnique({
-    // With user id from message
-    where: {
-      id: user_id,
-    },
-    // And selecting the hashed message representing the vote data
-    // select: {
-    //   hash: true,
-    // },
-  });
-  const hash = data.hash; // TESTING ONLY - delete this and uncomment the above select statement
+  if (isValidAuth(auth, secret)) {
+    // TESTING ONLY - MUDAMOS CLIENT BEHAVIOR
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 4096,
+      publicKeyEncoding: {
+        type: 'spki',
+        format: 'pem'
+      },
+      privateKeyEncoding: {
+        type: 'pkcs8',
+        format: 'pem',
+        cipher: 'aes-256-cbc',
+        passphrase: 'top secret'
+      }
+    });
 
-  if (user) {
-    const verify = crypto.createVerify('SHA256');
-    verify.write(data.hash);
-    verify.end();
+    const sign = crypto.createSign('SHA256');
+    sign.write(data.hash);
+    sign.end();
+    const signature = sign.sign({
+      key: privateKey,
+      passphrase: 'top secret',
+    }, 'hex');
+    // END TESTING CODE
 
-    const signature_verified = verify.verify(publicKey, signature, 'hex');
+    const user_id = data.message.split(';')[0];
 
-    const appSecret = "85f1f95aa0e94701caf1ebc7bffb96da8977071a7b92fe5410a1eb60886ebbb8";
-    const payload_hash = crypto
-      .createHmac("sha256", appSecret)
-      .update(data.message)
-      .digest("hex");
-    const message_verified = hash === payload_hash;
+    // Collect voter information
+    const user = await prisma.voters.findUnique({
+      // With user id from message
+      where: {
+        id: user_id,
+      },
+      // And selecting the hashed message representing the vote data
+      // select: {
+      //   hash: true,
+      // },
+    });
+    const hash = data.hash; // TESTING ONLY - delete this and uncomment the above select statement
 
-    if (signature_verified && message_verified) {
-      // Update voter object
-      await prisma.voters.update({
-        // With user id from message
-        where: { id: user_id },
-        // With signature and public key from payload
-        data: {
-          signature: signature.toString(),
-          public_key: publicKey.toString(),
-          hash: hash, // TESTING ONLY
-        },
-      });
-      // Upon success, respond with 200
-      res.status(200).send("Successful update");
+    if (user) {
+      const verify = crypto.createVerify('SHA256');
+      verify.write(data.hash);
+      verify.end();
+
+      const signature_verified = verify.verify(publicKey, signature, 'hex');
+
+      const payload_hash = crypto
+        .createHmac("sha256", process.env.APP_SECRET)
+        .update(data.message)
+        .digest("hex");
+      const message_verified = hash === payload_hash;
+
+      if (signature_verified && message_verified) {
+        // Update voter object
+        await prisma.voters.update({
+          // With user id from message
+          where: { id: user_id },
+          // With signature and public key from payload
+          data: {
+            signature: signature.toString(),
+            public_key: publicKey.toString(),
+            hash: hash, // TESTING ONLY
+          },
+        });
+        // Upon success, respond with 200
+        res.status(200).send("Successful update");
+      } else {
+        // If user does not exist, respond with 400
+        res.status(400).send("Invalid signature")
+      }
     } else {
       // If user does not exist, respond with 400
-      res.status(400).send("Invalid signature")
+      res.status(400).send("Invalid voter id")
     }
   } else {
-    // If user does not exist, respond with 400
-    res.status(400).send("Invalid voter id")
+    // If bearer token is invalid, respond with 401
+    res.status(401).send("Unauthorized")
   }
 };
