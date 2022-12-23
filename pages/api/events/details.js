cefa86ab3c20fe231c0261648d39028d96eef418
alexrandaccio <alex@radicalxchange.org>
2022-12-23 12:17:36 -0500

    correct & clean plural cluster match algorithm

--- pages/api/events/details.js
+++ pages/api/events/details.js
@@ -144,200 +144,82 @@ function generateStatistics(subjects, num_voters, credits_per_voter, voters) {
   };
 }
 
-function unique_members(g1, g2) {
+function clusterMatch(groups, contributions) {
+  // groups: bag of groups (array of arrays of integers)
+  // contributions: array of contributions (floats)
+  // groupMemberships: array of sets (arrays of arrays) where
+  //                   groupMemberships[i] is the groups agent i belongs to
+  let groupMemberships = new Array(contributions.length).fill([]);
 
-  // This helper function takes in two arrays, g1 and g2,
-  // and returns the elements that are just in g1 but not g2.
-
-  let unique = []
-  for (let i = 0; i < g1.length; i++) {
-    if (!g2.includes(g1[i])) {
-      unique.push(g1[i])
+  for (let i = 0; i < groups.length; i++) {
+    for (let j of groups[i]) {
+      groupMemberships[j] = [...groupMemberships[j], i];
     }
   }
-  return unique
-}
 
+  function commonGroup(i, j) {
+   // If voter i and voter j share any common group, return true. Else return false.
+   return groupMemberships[i].some(group => groupMemberships[j].includes(group))
+  }
 
-function count_memberships(groups, num_agents) {
-  /*
-  this function takes in a list of groups of the format described above and
-  a total number of agents, and returns a list called memberships where
-  memberships[i] is the number of groups that agent i is in.
-  */
-  let memberships = []
-  for (let i = 0; i < num_agents; i++) {
-    let num_memberships = 0
-    for (let g = 0; g < groups.length; g++) {
-      if (groups[g].indexOf(i) >= 0) {
-        num_memberships++
-      }
+  function K(i, group) {
+    // If group includes voter i, or any member of group shares a different group with voter i
+    if (group.includes(i) || group.some(j => commonGroup(i, j))) {
+      return Math.sqrt(contributions[i]);
     }
-    memberships.push(num_memberships)
+    return contributions[i];
   }
-  return memberships
-}
 
-function same_nonzero_sign(num1, num2) {
-  return ((num1 > 0) && (num2 > 0)) || ((num1 < 0) && (num2 < 0))
-}
+  let result = 0;
 
-function find_friends(groups, contributions) {
-  /*
-  this function takes in a list of groups and the total number of agents,
-  and returns a matrix / 2D array called friends where friends[i][j] = 1 if i
-  and j are in any group together AND have the same contribution polarity, and
-  0 otherwise.
-  */
-  let friends = []
-
-  for (let i = 0; i < contributions.length; i++) {
-    let new_row = new Array(contributions.length).fill(0)
-    friends.push(new_row)
-  }
-  /*
-  for every group, take each pair of people in that group, and make sure the
-  friends array is set to 1 at the index corresponding to that pair of people.
-  */
-  for (let g = 0; g < groups.length; g++) {
-    for (let i = 0; i < groups[g].length - 1; i++) {
-      for (let j = i + 1; j < groups[g].length; j++) {
-        if (same_nonzero_sign(contributions[groups[g][i]], contributions[groups[g][j]])) {
-          friends[groups[g][i]][groups[g][j]] = 1
-          friends[groups[g][j]][groups[g][i]] = 1
-        }
-      }
+  // Add the contribution of each agent in each group to the result
+  for (let g of groups) {
+    for (let i of g) {
+      result += contributions[i] / groupMemberships[i].length;
     }
   }
-  //console.log(friends)
-  return friends
-}
 
-// console.log(find_friends(groups, len(contributions)))
+  // Iterate over each pair of groups and add their contribution to the result
+  for (let g of groups) {
+    for (let h of groups) {
+      if (g === h) continue; // Skip if the groups are the same
 
-
-
-function root(x) {
-  /*
-  this function takes in a number x and returns sqrt(x) if x is positive, or
-  -sqrt(|x|) if x is negative.
-  */
-  return Math.sign(x) * Math.sqrt(Math.abs(x))
-}
-
-
-function cluster_match(groups, contributions) {
-  /*
-  first off, call helper functions to get a list showing the number of groups
-  each agent is in and a 2d array of which agents are in at least one group
-  together.
-  */
-
-  var memberships = count_memberships(groups, contributions.length)
-
-  var friends = find_friends(groups, contributions)
-
-  //console.log(memberships)
-
-  let funding_amount = 0
-
-  // first just sum up all the individual contributions
-  contributions.forEach(x => {
-    funding_amount += x
-  })
-
-  //console.log("sum of individual contributions: " + funding_amount)
-
-  // now add in all the interaction terms
-  for (let g = 0; g < groups.length - 1; g++) {
-    for (let h = g + 1; h < groups.length; h++) {
-
-      //console.log("finding interaction term for groups " + g + " and " + h)
-
-      /*
-      we need to figure out which members of g and h are friends with each
-      other. this for loop does that. it fills up a list called common_friends
-      with all the agents who are friends with at least one person from the
-      other group. (it actually might add agents to this list more than once,
-      which is fine for our purposes)
-      */
-      let common_friends = []
-      for (let i = 0; i < groups[g].length; i++) {
-        for (let j = 0; j < groups[h].length; j++) {
-          if (friends[groups[g][i]][groups[h][j]] == 1) {
-            common_friends.push(groups[g][i])
-            common_friends.push(groups[h][j])
-          }
-        }
+      let term1 = 0;
+      // Calculate term1 for the current pair of groups
+      for (let i of g) {
+        term1 += K(i, h) / groupMemberships[i].length;
       }
+      term1 = Math.sqrt(term1);
 
-      // g_term and h_term are g and h's sides of the interaction term, respectively.
-      // they'll get multiplied together to form the full interaction term.
-      let g_term = 0;
-
-      for (let i = 0; i < groups[g].length; i++) {
-        if (common_friends.includes(groups[g][i])) {
-          g_term += root(contributions[groups[g][i]]) / memberships[groups[g][i]]
-
-        } else {
-          g_term += contributions[groups[g][i]] / memberships[groups[g][i]]
-        }
+      let term2 = 0;
+      // Calculate term2 for the current pair of groups
+      for (let j of h) {
+        term2 += K(j, g) / groupMemberships[j].length;
       }
+      term2 = Math.sqrt(term2);
 
-      let h_term = 0;
-
-      for (let j = 0; j < groups[h].length; j++) {
-        if (common_friends.includes(groups[h][j])) {
-          h_term += root(contributions[groups[h][j]]) / memberships[groups[h][j]]
-          //console.log(contributions[groups[h][j]], '/', memberships[groups[h][j]])
-        } else {
-          h_term += contributions[groups[h][j]] / memberships[groups[h][j]]
-        }
-      }
-
-      /*
-      now, add the interaction term to the total funding amount.
-      if the two terms are signed differently, then one group likes the
-      proposal/project and the other doesn't. In this case, it's not
-      immediately clear how to change the funding amount, so we just won't.
-      */
-      if (Math.sign(g_term) == Math.sign(h_term)) {
-        funding_amount += 2 * root(g_term) * root(h_term) * Math.sign(g_term)
-
-        // if both g_term and h_term were positive, then we could just write:
-        // funding_amount += 2 * root(g_term) * root(h_term)
-        // but if they're both negative, we don't want those two negative
-        // numbers to cancel out and make a positive number. So we multiply again by Math.sign(g_term).
-      }
+      result += term1 * term2;
     }
   }
-  return funding_amount
+
+  return Math.sqrt(result);
 }
 
 function calculatePluralVotes(groups, contributions) {
-  console.log("groups")
-  console.log(groups)
-  console.log("contributions")
-  console.log(contributions)
-  var pos_contributions = []
-  var neg_contributions = []
+  var positiveContributions = [];
+  var negativeContributions = [];
   for (let i = 0; i < contributions.length; i++) {
     if (contributions[i] >= 0) {
-      pos_contributions.push(contributions[i])
-      neg_contributions.push(0)
+      positiveContributions.push(contributions[i]);
+      negativeContributions.push(0);
     } else {
-      pos_contributions.push(0)
-      neg_contributions.push(contributions[i])
+      positiveContributions.push(0);
+      negativeContributions.push(Math.abs(contributions[i]));
     }
   }
-  // console.log(pos_contributions)
-  // console.log(neg_contributions)
-  // console.log(groups)
-  // console.log(cluster_match(groups, pos_contributions))
-  // console.log(cluster_match(groups, neg_contributions))
-  let pos_amount = cluster_match(groups, pos_contributions)
-  let neg_amount = cluster_match(groups, neg_contributions)
-  return root(pos_amount) + root(neg_amount)
+  let positiveAmount = clusterMatch(groups, positiveContributions);
+  let negativeAmount = clusterMatch(groups, negativeContributions);
+  return positiveAmount - negativeAmount;
 }
 
 /**
@@ -395,13 +277,10 @@ function generateStatisticsPlural(subjects, num_voters, credits_per_voter, voter
       contributions[j] = [...contributions[j], Math.pow(voter_data[j].votes, 2)];
     }
   }
-  console.log(groups);
-  console.log(contributions);
 
   for (let i = 0; i < contributions.length; i++) {
     finalVotes[i] = calculatePluralVotes(groups.map((group) => group.members), contributions[i]);
   }
-  console.log(finalVotes);
 
   // Return computed statistics
   return {
